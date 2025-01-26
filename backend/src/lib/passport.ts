e4f8bd8cae65d5237162375173c3a169cab4f6b7
Роман Максимов <r.maximov@banki.ru>
2025-01-27 00:36:26 +0300

    ignore auth error

--- backend/src/lib/passport.ts
+++ backend/src/lib/passport.ts
@@ -31,6 +31,9 @@ export const applyPassportToExpressApp = (expressApp: Express, ctx: AppContext) 
       return next();
     }
 
-    passport.authenticate('jwt', { session: false })(req, res, next);
+    passport.authenticate('jwt', { session: false }, (...args: unknown[]) => {
+      req.user = args[1] || undefined;
+      next();
+    })(req, res, next);
   });
 };

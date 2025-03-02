4684e75bd225910c1f5b4e9f9b83ca8f21d86038
Роман Максимов <r.maximov@banki.ru>
2025-03-02 23:34:00 +0300

    infinity scroll

--- frontend/src/screens/AllIdeasPage/AllIdeadPage.tsx
+++ frontend/src/screens/AllIdeasPage/AllIdeadPage.tsx
@@ -2,43 +2,66 @@ import { Link } from "react-router-dom";
 import { trpc } from "../../lib/trpc";
 import { getIdeaRoute } from "../../lib/routes";
 import { Segment } from '../../components/Segment';
+import { Alert } from '../../components/Alert';
 
 import s from './AllIdeasPage.module.scss';
 
 export const AllIdeasPage = () => {
     const {
-        data, error, isLoading, isFetching, isError,
-    } = trpc.getIdeas.useQuery();
+        data, error, isLoading, isError,
+        hasNextPage, fetchNextPage, isFetchingNextPage, isRefetching,
+    } = trpc.getIdeas.useInfiniteQuery({ limit: 2 }, {
+        getNextPageParam: (lastPage) => {
+            return lastPage.nextCursor;
+        },
+    });
 
-    if (isLoading || isFetching) {
-        return <div>loading...</div>
-    }
+    const renderContent = () => {
+        if (isLoading || isRefetching) {
+            return <div>loading...</div>;
+        }
 
-    if (isError) {
-        return <div>{error.message}</div>
-    }
+        if (isError) {
+            return <Alert color="red">{error.message}</Alert>;
+        }
 
-    if (!data) {
-      return <div>no ideas at all</div>
+        return (
+          <ul className={s.ideas}>
+              {data.pages
+                .flatMap((page) => page.ideas)
+                .map((idea) => (
+                    <div key={idea.nick} className={s.idea}>
+                        <Segment
+                          size={2}
+                          title={(
+                            <Link to={getIdeaRoute({ nick: idea.nick })} className={s.ideaLink}>
+                                {idea.name}
+                            </Link>
+                          )}
+                          description={idea.description}
+                        />
+                    </div>
+                  ))
+              }
+
+              <div className={s.more}>
+                  {hasNextPage && !isFetchingNextPage && (
+                    <button type="button" onClick={() => {
+                        void fetchNextPage();
+                    }}>
+                        Load more
+                    </button>
+                  )}
+
+                  {isFetchingNextPage && <div>loading...</div>}
+              </div>
+          </ul>
+        );
     }
 
     return (
         <Segment title="All Ideas">
-            <ul className={s.ideas}>
-                {data.ideas.map((idea) => (
-                    <div key={idea.nick} className={s.idea}>
-                      <Segment
-                        size={2}
-                        title={(
-                          <Link to={getIdeaRoute({ nick: idea.nick })} className={s.ideaLink}>
-                            {idea.name}
-                          </Link>
-                        )}
-                        description={idea.description}
-                      />
-                    </div>
-                ))}
-            </ul>
+            {renderContent()}
         </Segment>
     );
 };
